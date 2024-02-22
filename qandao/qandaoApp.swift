094900e6668b1714ef9167e216da24dd3d2f8218
bill donner <billdonner@gmail.com>
2024-02-22 07:16:13 -0600

    Initial Commit

---
+++ qandao/qandaoApp.swift
@@ -0,0 +1,17 @@
+//
+//  qandaoApp.swift
+//  qandao
+//
+//  Created by bill donner on 2/22/24.
+//
+
+import SwiftUI
+
+@main
+struct qandaoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
